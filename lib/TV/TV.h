d5a2e026c9e7b6b4b04027b26b436f39ae9466d7
Your Name <you@example.com>
2019-02-23 11:51:19 +0200

    Set default values to some arguments

--- lib/TV/TV.h
+++ lib/TV/TV.h
@@ -13,11 +13,11 @@ public:
      int          volumeDesired,
      String       playbackAction);
 
-  bool powerOn;
-  unsigned int channelNumber;
-  bool muted;
-  int volume;
-  int volumeDesired;
-  String playbackAction;
+  bool powerOn               = false;
+  unsigned int channelNumber = 0;
+  bool muted                 = false;
+  int volume                 = -1;
+  int volumeDesired          = -1;
+  String playbackAction      = "";
 };
 #endif // ifndef TV_H
